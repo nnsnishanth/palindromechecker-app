81b0796fd54e73c4e9930d96182ef905ffdc3d40
nnsnishanth <nnsnishanth@gmail.com>
2026-02-25 15:58:07 +0530

    Restructured project to proper src layout

--- untitled/src/PalindromeCheckerApp.java
+++ untitled/src/PalindromeCheckerApp.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+public class PalindromeCheckerApp { */
+    public static void runUC2() {
 
-public class PalindromeCheckerApp {
+        System.out.println("---- UC2: Half-Length Comparison ----");
+
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome: " + isPalindrome + "\n");
+    }
+
+/**
 }
